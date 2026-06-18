aee7a2f5ddb152260b50dbca7117f1fc7ab8022f
gayathri-the-developer <gayathrithankappan27@gmail.com>
2026-06-18 11:04:36 +0530

    Add files via upload

---
+++ pyth.py
@@ -0,0 +1,11 @@
+name="gayu"
+age="20"
+print(f"my name is {name} and my age is {20}")
+
+
+name=input("Enter your name")
+print(name)
+age=input("Enter your age")
+print(age)
+subject=input("Enter your fav subject")
+print(subject)
